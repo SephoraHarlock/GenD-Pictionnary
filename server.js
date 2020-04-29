18b18a5e3202e0d3b6d0e37035a9c428a8bc6afb
Lua Jacqmin <lua.jacqmin@student.heaj.be>
2020-04-29 12:40:39 +0200

    add tour player

--- server.js
+++ server.js
@@ -18,13 +18,23 @@ io.on('connection', (socket) => {
 })
 
 let users = [];
+let currentPlayer = null;
+let timeout;
+let words = ['potato', 'witch', 'ps5']
 
 function onConnection(socket) {
   socket.on('username', (username) => {
     console.log('Client name :', username);
     socket.username = username;
-    users.push(socket);
 
+    if(users.length === 0){
+      currentPlayer = socket;
+      timeout = clearTimeout(timeout);
+      users.push(socket);
+      switchPlayer();
+    } else {
+      users.push(socket);
+    }
     sendUsers();
   })
 
@@ -37,11 +47,31 @@ function onConnection(socket) {
       return user !== socket;
     });
     sendUsers();
+
+    if(users.length === 0){
+      timeout = clearTimeout(timeout);
+    }
   })
 }
 
 function sendUsers() {
   io.emit('users', users.map((user) => {
-    return user.username;
+    return {
+      username : user.username,
+      active : user === currentPlayer
+    }
   }))
+}
+
+function switchPlayer() {
+  if(users.length === 0) return;
+
+  const indexCurrentPlayer = users.indexOf(currentPlayer);
+  currentPlayer = users[(indexCurrentPlayer + 1) % users.length];
+
+  sendUsers();
+  timeout = setTimeout(switchPlayer, 20000);
+
+  currentPlayer.emit('word', words[Math.floor(words.length * Math.random())])
+  io.emit('clear');
 }
